b4e3b15a256d49e057d38ddcdf2f6b500e53f659
tahereshamimi <tahereshamimi@gmail.com>
2019-03-10 15:13:26 +0330

    add slider

--- asset/js/script.js
+++ asset/js/script.js
@@ -15,11 +15,13 @@ $(document).ready(function () {
   });
 
   $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    items:1,
-    dots:false
-  })
+    loop:true,
+    margin:10,
+    nav:true,
+    items: 1,
+    dots:false,
+    rtl: true,
+    singleItem: true
+  });
 });
 
