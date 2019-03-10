37303dffb47f45ad1d1f806d32187593e1ab1f13
tahereshamimi <tahereshamimi@gmail.com>
2019-03-10 12:26:53 +0330

    slider add but white slide

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,9 +1,25 @@
 $(document).ready(function () {
   $('.movie-img').css('height', $('.movie-img').width() * 1.5);
-  $("#movieFilter").on("keyup", function() {
+  $("#movieFilter").on("keyup", function () {
     var value = $(this).val().toLowerCase();
-    $(".movies .movie").filter(function() {
+    $(".movies .movie").filter(function () {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
+
+  $("#movieFilter").click(function () {
+    $('html,body').animate({
+      scrollTop: $("#movieFilter").offset().top
+    },
+      'slow');
+  });
+
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    items:1,
+    dots:false
+  })
 });
+
