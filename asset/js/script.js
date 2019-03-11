c1b3dce583b36d1d82643371a15adc812049a9da
tahereshamimi <tahereshamimi@gmail.com>
2019-03-11 12:29:04 +0330

    hoverForMore added

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
-  $('.movie-img').css('height', $('.movie-img').width() * 1.5);
+  let imgWidth=$('.movie-img').width();
+  console.log(imgWidth)
+  let imgHeight=imgWidth*1.5;
+  
+  $('.movie-img').css('height',imgHeight);
+
   $("#movieFilter").on("keyup", function () {
     var value = $(this).val().toLowerCase();
     $(".movies .movie").filter(function () {
@@ -22,6 +27,24 @@ $(document).ready(function () {
     dots:false,
     rtl: true,
     singleItem: true
+  });
+
+
+
+  // overflow
+  $(".overflowing").hoverForMore({
+    speed: 60.0,		// Measured in pixels-per-second
+    loop: true,		// Scroll to the end and stop, or loop continuously?
+    gap: 20,		// When looping, insert this many pixels of blank space
+    target: false,		// Hover on this CSS selector instead of the text line itself
+    removeTitle: true,	// By default, remove the title attribute, as a tooltip is redundant
+    snapback: true,		// Animate when de-activating, as opposed to instantly reverting
+    addStyles: true,	// Auto-add CSS; leave this on unless you need to override default styles
+    alwaysOn: false,	// If you're insane, you can turn this into a <marquee> tag. (Please don't.)
+  
+    // In case you want to alter the events which activate and de-activate the effect:
+    startEvent: "mouseenter",
+    stopEvent: "mouseleave"
   });
 });
 
